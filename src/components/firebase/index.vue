a3c2c885c6ec63049b304fab0a8e250db4eeccd5
Tran Thanh Loi <nhoxzunbmt@gmail.com>
2018-03-13 23:08:31 +0700

    U

--- src/components/firebase/index.vue
+++ src/components/firebase/index.vue
@@ -1,11 +1,51 @@
 <template>
     <div>
 
-    <input type="text" v-model="title" placeholder="Title">
 
-        <input type="text" v-model="content" placeholder="Content">
+  <v-container fluid>
+    <v-layout row>
+      <v-flex xs6>
 
-            <button @click="addNote">Add</button>
+    <v-text-field
+          name="title"
+          label="Title"
+          id="title"
+          v-model="title"
+        ></v-text-field>
+
+    <v-text-field
+          name="content"
+          label="Content"
+          id="content"
+          v-model="content"
+        ></v-text-field>
+
+
+            <v-btn color="success" @click="addNote">Add</v-btn>
+
+<v-data-table
+      :headers="headers"
+      :items="notes"
+      hide-actions
+      class="elevation-1"
+    >
+      <template slot="items" slot-scope="props">
+        <td>{{ props.item.title }}</td>
+        
+         <td class="text-xs-center">{{ props.item.content }}</td>
+
+            <td class="justify-center layout px-0">
+          <v-btn icon class="mx-0" @click="editItem(props.item)">
+            <v-icon color="teal">edit</v-icon>
+          </v-btn>
+          <v-btn icon class="mx-0" @click="deleteItem(props.item)">
+            <v-icon color="pink">delete</v-icon>
+          </v-btn>
+        </td>
+      </template>
+    </v-data-table>
+
+
 
             <ul class="notes_items">
                 <li v-for="note in notes" :key="note.id">
@@ -15,6 +55,9 @@
                     <button @click="deleteNote(note.id)">Delete</button>
                 </li>
             </ul>
+      </v-flex>
+    </v-layout>
+  </v-container>
     </div>
 </template>
 
@@ -26,7 +69,22 @@ export default {
   data() {
     return {
       title: "",
-      content: ""
+      content: "",
+      headers: [
+        {
+          text: 'Title',
+          align: 'left',
+          sortable: false,
+          value: 'name'
+        },
+        {
+          text: 'Content',
+          align: 'center',
+          sortable: false,
+          value: 'content'
+        },
+            { text: 'Actions', value: 'name', sortable: false        ,align: 'center' }
+      ]
     };
   },
   firebase: {
